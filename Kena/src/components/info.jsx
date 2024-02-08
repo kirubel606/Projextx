05a79aefb5bace172337dc5d7a1911c391214941
kirubel606 <kiruabatu@gmail.com>
2024-02-08 14:46:33 +0300

    ..

--- Kena/src/components/info.jsx
+++ Kena/src/components/info.jsx
@@ -2,27 +2,47 @@ import * as React from "react";
 
 function Info (){
     return(
-        <div className="row" style={{ height: "100vh" }}>
-       
-        <p className="col-md-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-          facere ipsam, vitae eveniet architecto et, amet rerum minus commodi
-          corrupti inventore aperiam recusandae porro. Quas, rem! Corrupti
-          delectus soluta fugit. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Fugit eveniet neque illum nemo itaque aliquid dolores
-          doloribus. Fugit modi facere beatae vel amet explicabo soluta
-          accusantium, culpa totam quidem quia! Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Inventore repellendus voluptas obcaecati
-          vitae, maxime voluptate iusto facere qui doloremque! Eaque assumenda
-          molestias quaerat optio autem dolorum aut enim cupiditate tempore. Lorem
-          ipsum dolor sit amet, consectetur adipisicing elit. Ullam hic corporis
-          error repudiandae, sequi assumenda cupiditate nobis rem voluptates
-          cumque cum nesciunt fuga, dolor nulla consectetur illum eveniet
-          perferendis ea! Lorem ipsum dolor sit, amet consectetur adipisicing
-          elit. Harum enim dignissimos mollitia! Saepe, veniam non, animi
-          quibusdam placeat eligendi eaque sit fugiat doloremque praesentium ea
-          minima temporibus, ad id fuga!Lorem
-        </p>
+    <div className="container">
+        <div className="row" style={{ height: "100vh", marginTop:"200px" }}>
+
+            <div className="col-md-6">
+                <p>image</p>
+            </div>
+
+            <div className="col-md-6" >
+                <p> molestias quaerat optio autem dolorum aut enim cupiditate tempore. Lorem
+                ipsum dolor sit amet, consectetur adipisicing elit. Ullam hic corporis
+                error repudiandae, sequi assumenda cupiditate nobis rem voluptates
+                cumque cum nesciunt fuga, dolor nulla consectetur illum eveniet
+                perferendis ea! Lorem ipsum dolor sit, amet consectetur adipisicing
+                elit. Harum enim dignissimos mollitia! Saepe, veniam non, animi
+                quibusdam placeat eligendi eaque sit fugiat doloremque praesentium ea
+                minima temporibus, ad id fuga!Lorem
+                </p>
+            </div>
+            
+        </div>
+
+
+        <div className="row" style={{ height: "100vh", paddingTop:"200px" }}>
+
+                <div className="col-md-6">
+                    <p>image2</p>
+                </div>
+
+                <div className="col-md-6">
+                    <p> molestias quaerat optio autem dolorum aut enim cupiditate tempore. Lorem
+                    ipsum dolor sit amet, consectetur adipisicing elit. Ullam hic corporis
+                    error repudiandae, sequi assumenda cupiditate nobis rem voluptates
+                    cumque cum nesciunt fuga, dolor nulla consectetur illum eveniet
+                    perferendis ea! Lorem ipsum dolor sit, amet consectetur adipisicing
+                    elit. Harum enim dignissimos mollitia! Saepe, veniam non, animi
+                    quibusdam placeat eligendi eaque sit fugiat doloremque praesentium ea
+                    minima temporibus, ad id fuga!Lorem
+                    </p>
+                </div>
+
+        </div>
       </div>
     );
 }
